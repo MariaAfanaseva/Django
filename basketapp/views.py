6c9f14d67acce3de85a02989ca8a643ebe85045b
Maria Afanaseva <mashenkachukina@gmail.com>
2019-04-12 00:27:51 +0200

    Lesson_6

--- basketapp/views.py
+++ basketapp/views.py
@@ -1,13 +1,21 @@
-from django.shortcuts import render, HttpResponseRedirect, get_object_or_404
+from django.shortcuts import render, HttpResponseRedirect, get_object_or_404, HttpResponse
+from django.contrib.auth.decorators import login_required
 from basketapp.models import Basket
 from mainapp.models import Product
+from django.urls import reverse
 
 
+@login_required
 def basket(request):
-    context = {}
+    title = 'Basket'
+    basket_items = Basket.objects.filter(user=request.user).order_by('product__category')
+    context = {'basket_items': basket_items,
+               'title': title,
+               }
     return render(request, 'basketapp/basket.html', context)
 
 
+@login_required
 def basket_add(request, pk):
     product = get_object_or_404(Product, pk=pk)
     basket = Basket.objects.filter(user=request.user, product=product).first()
@@ -17,18 +25,34 @@ def basket_add(request, pk):
     basket.quantity += 1
     basket.save()
 
+    if 'login' in request.META.get('HTTP_REFERER'):
+        return HttpResponseRedirect(reverse('products:product', args=[pk]))
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
+@login_required
 def basket_remove(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    basket = Basket.objects.filter(user=request.user, product=product).first()
+    basket_record = Basket.objects.filter(user=request.user, pk=pk).first()
 
     if basket:
-        if basket.quantity > 1:
-            basket.quantity -= 1
-            basket.save()
-        else:basket.delete()
+        if basket_record.quantity > 1:
+            basket_record.quantity -= 1
+            basket_record.save()
+        else: basket_record.delete()
 
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+@login_required
+def basket_edit(request, pk):
+    quantity = int(request.GET.get('quantity'))
+    basket_record = get_object_or_404(Basket, pk=pk)
+
+    if quantity > 0:
+        basket_record.quantity = quantity
+        basket_record.save()
+    else:
+        basket_record.delete()
+
+    return HttpResponse('ok')
 
