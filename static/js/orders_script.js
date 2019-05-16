90a2e3fd3ec1e6dab8f1a89343d44998629b7730
Maria Afanaseva <mashenkachukina@gmail.com>
2019-05-16 18:02:06 +0200

    Added formset.js - remove products and add new product in order-form

--- static/js/orders_script.js
+++ static/js/orders_script.js
@@ -12,55 +12,71 @@ window.onload = function () {
     var orderTotalCost = parseFloat($orderTotalCost.text().replace(',', '.')) || 0;
 
     function orderSummaryUpdate(orderitemPrice, deltaQuantity) {
-       deltaCost = orderitemPrice * deltaQuantity;
+        deltaCost = orderitemPrice * deltaQuantity;
 
-       orderTotalCost = Number((orderTotalCost + deltaCost).toFixed(2));
-       orderTotalQuantity = orderTotalQuantity + deltaQuantity;
+        orderTotalCost = Number((orderTotalCost + deltaCost).toFixed(2));
+        orderTotalQuantity = orderTotalQuantity + deltaQuantity;
 
-       $orderTotalCost.html(orderTotalCost.toString());
-       $orderTotalQuantity.html(orderTotalQuantity.toString());
+        $orderTotalCost.html(orderTotalCost.toString());
+        $orderTotalQuantity.html(orderTotalQuantity.toString());
     }
 
+    function deleteOrderItem(row) {
+        var targetName = row[0].querySelector('input[type="number"]').name;
+        orderitemNum = parseInt(targetName.replace('orderitems-', '').replace('-quantity', ''));
+        deltaQuantity = -quantityArr[orderitemNum];
+        orderSummaryUpdate(priceArr[orderitemNum], deltaQuantity);
+    }
+
+
     for (i = 0; i < TOTAL_FORMS; i++) {
-       _quantity = parseInt($('input[name="orderitems-' + i + '-quantity"]').val());
-       _price = parseFloat($('.orderitems-' + i + '-price').text().replace(',', '.'));
-       quantityArr[i] = _quantity;
-       if (_price) {
-           priceArr[i] = _price;
-       } else {
-           priceArr[i] = 0;
-       }
+        _quantity = parseInt($('input[name="orderitems-' + i + '-quantity"]').val());
+        _price = parseFloat($('.orderitems-' + i + '-price').text().replace(',', '.'));
+        quantityArr[i] = _quantity;
+        if (_price) {
+            priceArr[i] = _price;
+        } else {
+            priceArr[i] = 0;
+        }
     }
 
     if (!orderTotalQuantity) {
-       for (i = 0; i < TOTAL_FORMS; i++) {
-           orderTotalQuantity += quantityArr[i];
-           orderTotalCost += quantityArr[i] * priceArr[i];
-       }
-       $orderTotalQuantity.html(orderTotalQuantity.toString());
-       $orderTotalCost.html(Number(orderTotalCost.toFixed(2)).toString());
+        for (i = 0; i < TOTAL_FORMS; i++) {
+            orderTotalQuantity += quantityArr[i];
+            orderTotalCost += quantityArr[i] * priceArr[i];
+        }
+        $orderTotalQuantity.html(orderTotalQuantity.toString());
+        $orderTotalCost.html(Number(orderTotalCost.toFixed(2)).toString());
     }
 
     $orderForm.on('change', 'input[type="number"]', function (event) {
-       var target = event.target;
-       orderitemNum = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
-       if (priceArr[orderitemNum]) {
-           orderitemQuantity = parseInt(target.value);
-           deltaQuantity = orderitemQuantity - quantityArr[orderitemNum];
-           quantityArr[orderitemNum] = orderitemQuantity;
-           orderSummaryUpdate(priceArr[orderitemNum], deltaQuantity);
-       }
+        var target = event.target;
+        orderitemNum = parseInt(target.name.replace('orderitems-', '').replace('-quantity', ''));
+        if (priceArr[orderitemNum]) {
+            orderitemQuantity = parseInt(target.value);
+            deltaQuantity = orderitemQuantity - quantityArr[orderitemNum];
+            quantityArr[orderitemNum] = orderitemQuantity;
+            orderSummaryUpdate(priceArr[orderitemNum], deltaQuantity);
+        }
     });
 
     $orderForm.on('change', 'input[type="checkbox"]', function (event) {
-       var target = event.target;
-       orderitemNum = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
-       if (target.checked) {
-           deltaQuantity = -quantityArr[orderitemNum];
-       } else {
-           deltaQuantity = quantityArr[orderitemNum];
-       }
-       orderSummaryUpdate(priceArr[orderitemNum], deltaQuantity);
+        var target = event.target;
+        orderitemNum = parseInt(target.name.replace('orderitems-', '').replace('-DELETE', ''));
+        if (target.checked) {
+            deltaQuantity = -quantityArr[orderitemNum];
+        } else {
+            deltaQuantity = quantityArr[orderitemNum];
+        }
+        orderSummaryUpdate(priceArr[orderitemNum], deltaQuantity);
     });
+
+    $('.formset_row').formset({
+        addText: 'Add product',
+        deleteText: 'Delete',
+        prefix: 'orderitems',
+        removed: deleteOrderItem
+    });
+
 
 };
