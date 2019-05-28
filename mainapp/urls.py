b7aea30063e7ab7692272f9371208d39438a3245
Maria Afanaseva <mashenkachukina@gmail.com>
2019-05-28 13:53:21 +0200

    cache page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -7,7 +7,7 @@ app_name = 'mainapp'
 urlpatterns = [
     url(r'^(?P<num>\d+)$', mainapp.products, name='index'),
     url(r'^category/(?P<pk>\d+)/$', mainapp.products, name='category'),
-    url(r'^category/(?P<pk>\d+)/$', cache_page(3600)(mainapp.products_ajax)),
+    url(r'^category/(?P<pk>\d+)/ajax/$', cache_page(3600)(mainapp.products_ajax)),
     url(r'^category/(?P<pk>\d+)/page/(?P<page>\d+)/$', mainapp.products, name='page'),
     url(r'^category/(?P<pk>\d+)/page/(?P<page>\d+)/ajax/$', cache_page(3600)(mainapp.products_ajax)),
     url(r'^product/(?P<pk>\d+)/$', mainapp.product, name='product'),
