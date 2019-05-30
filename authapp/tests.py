96589b2b8d75260d1fe597236530ec7b842a4da0
Maria Afanaseva <mashenkachukina@gmail.com>
2019-05-30 21:55:14 +0200

    Added test authapp(logout)

--- authapp/tests.py
+++ authapp/tests.py
@@ -42,6 +42,24 @@ class TestUserManagement(TestCase):
         self.assertEqual(response.context['user'], self.user)
         # self.assertIn('Пользователь', response.content.decode())
 
+    def test_user_logout(self):
+        # данные пользователя
+        self.client.login(username='tarantino', password='geekbrains')
+
+        # логинимся
+        response = self.client.get('/auth/login/')
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(response.context['user'].is_anonymous)
+
+        # выходим из системы
+        response = self.client.get('/auth/logout/')
+        self.assertEqual(response.status_code, 302)
+
+        # главная после выхода
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.context['user'].is_anonymous)
+
     def test_basket_login_redirect(self):
         # без логина должен переадресовать
         response = self.client.get('/basket/')
